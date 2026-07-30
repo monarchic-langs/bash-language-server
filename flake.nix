3f407c3c10fc9f6e44858e6cc9d51e0c07a7eca9
midischwarz12 <midischwarz@proton.me>
2026-07-29 21:52:10 -0500

    ci: align bash workflows with fleet policy

--- flake.nix
+++ flake.nix
@@ -7,10 +7,37 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    formatter.${system} = pkgs.alejandra;
-    packages.${system}.default = pkgs.bash-language-server;
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [pkgs.bash-language-server pkgs.pnpm];
+    formatter = {
+      ${system} = pkgs.alejandra;
+    };
+    packages = {
+      ${system}.default = pkgs.bash-language-server;
+    };
+    checks = {
+      ${system} = {
+        default = pkgs.bash-language-server;
+
+        flake-format =
+          pkgs.runCommand "bash-language-server-flake-format-check"
+          {nativeBuildInputs = [pkgs.alejandra];}
+          ''
+            alejandra --check ${./flake.nix}
+            touch $out
+          '';
+
+        bash-language-server-version =
+          pkgs.runCommand "bash-language-server-version-check"
+          {nativeBuildInputs = [pkgs.bash-language-server];}
+          ''
+            bash-language-server --version
+            touch $out
+          '';
+      };
+    };
+    devShells = {
+      ${system}.default = pkgs.mkShell {
+        packages = [pkgs.bash-language-server pkgs.pnpm];
+      };
     };
   };
 }
